77216f4970c99f7e3d4776ff91a50b4656b79bd6
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-06-25 23:07:29 +0200

    Enforce location connections when going to a new one

--- src/commands/go.rs
+++ src/commands/go.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 use crate::{
     locations::{CurLocation, Location},
     output::animate_typing,
+    relations::LocationConnection,
 };
 
 /// The player issued a go command
@@ -11,6 +12,7 @@ pub struct GoEvent(pub String);
 pub fn handle_go(
     mut ev_go: EventReader<GoEvent>,
     mut q_cur_location: Query<&mut CurLocation>,
+    q_location_connections: Query<&LocationConnection>,
     q_locations: Query<(Entity, &Name), With<Location>>,
 ) {
     for ev in ev_go.iter() {
@@ -18,25 +20,42 @@ pub fn handle_go(
         let mut cur_location = q_cur_location.single_mut();
         let mut new_location: Option<(Entity, String)> = None;
 
-        for (entity, name) in q_locations.iter() {
-            let name = name.as_str().to_uppercase();
+        // Check which locations are connected to the current one
+        for connection in q_location_connections.iter() {
+            if let Some(connected_entity) = connection.connected_location(cur_location.0) {
+                // Check if the connected location matches the name
+                if let Ok((entity, name)) = q_locations.get(connected_entity) {
+                    let name = name.as_str().to_uppercase();
 
-            // Go to the new location
-            if name == target {
-                new_location = Some((entity, name))
+                    // Go to the new location
+                    if name == target {
+                        new_location = Some((entity, name))
+                    }
+                }
             }
         }
 
         if let Some((new_location, name)) = new_location {
-            if cur_location.0 == new_location {
-                // The player wants to go to the same location
-                animate_typing(format!("You are already at {name}!"));
-            } else {
-                // Change the location
-                cur_location.0 = new_location;
-                animate_typing(format!("You are now at {name}!"));
-            }
+            // Change the location
+            cur_location.0 = new_location;
+            animate_typing(format!("You are now at {name}!"));
         } else {
+            // Check if the player is already at the location
+            if let Ok((_, name)) = q_locations.get(cur_location.0) {
+                if target == name.as_str().to_uppercase() {
+                    animate_typing(format!("You are already at {target}!"));
+                    return;
+                }
+            }
+
+            // Check if a location with this name exists at all
+            for (_, name) in q_locations.iter() {
+                if target == name.as_str().to_uppercase() {
+                    animate_typing(format!("You can't get to {target} from here!"));
+                    return;
+                }
+            }
+
             // Invalid location name
             animate_typing(format!("I don't know what {target} is!"));
         }
